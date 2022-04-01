e86caaa4d14de9a67ed9088e16ce879895eebdeb
Kallu <hkallu@teksystems.com>
2022-04-01 13:02:51 +0530

    hi

---
+++ demo1/src/list/list1.java
@@ -0,0 +1,17 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class list1 {
+	public static void main(String[] args) {
+		List<String> str=new ArrayList<String>();
+		str.add("hi");
+		str.add("people");
+		str.add("i am hari");
+		for (String s :str)
+			System.out.println(s);
+				
+	}
+
+}
